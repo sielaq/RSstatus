0830b2fa1981eefe2fd83c5708af843753505bbe
sielaq <sielaq@gmail.com>
2021-11-16 11:59:44 +0100

    Add new features
    * support colors (colorize PRIMARY and current host)
    * support being sourced (like by profile.d)
    * autodetect column version (counting trick)
    * puryfy JSON (in case of WARNINGS)

--- rsstatus.sh
+++ rsstatus.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ "${BASH_SOURCE[0]}" != "${0}" ] && alias exit=return
+
+PORT=27017
 
 helpJSHON() {
   exec 1>&2
@@ -34,12 +37,26 @@ help() {
   echo "Just run $0"
 }
 
+GRN=$(echo -e "\033[32m")
+END=$(echo -e "\033[0m")
+
+supportColors() {
+  numberOfChars=$(echo -e "${GRN}X${END}\t0\nX\t0" | column -t -x | wc -c)
+  [ $numberOfChars -eq 19 ] && return 0
+  return 1
+}
+
+colorize() {
+  supportColors && _GRN=$GRN && _END=$END
+  sed 's/'$1'/'${_GRN}${1}${_END}'/g'
+}
+
 useMongoSSL() {
   mongo -h | grep -q ssl
 }
 
 checkSSL () {
-  echo | openssl s_client -connect $1:27017 >/dev/null 2>&1
+  echo | openssl s_client -connect :$PORT >/dev/null 2>&1
 }
 
 shortHost() {
@@ -58,6 +75,10 @@ fixHidden() {
   sed 's/null//g;
        s/false//g;
        s/true/(hidden)/g'
+}
+
+fixColors() {
+  sed -r 's/\x1B\[(;?[0-9]{1,3})+[mGK]//g'
 }
 
 getDataFromJSON() {
@@ -88,7 +109,7 @@ fixLine() {
   while read data; do
     printf "| "
     printf "$data"
-    maxLine=$(echo "$data" | wc -L)
+    maxLine=$(echo "$data" | fixColors | wc -L)
     countMissing=$(( $1 - $maxLine ))
     for i in $(seq 1 $countMissing); do
       printf " "
@@ -107,11 +128,18 @@ bottomLine() {
 }
 
 separateColumns() {
-  sed 's/ \([-0-9a-zA-Z(]\)/| \1/g'
+  sed 's/ \([-0-9a-zA-Z('$END']\)/| \1/g'
 }
 
 adjustColumns() {
   column -t -x
+}
+
+purifyJSON() {
+  eval dirtyJSON=$(echo \$"$1")
+  export $1="$(echo "$dirtyJSON" | grep -v ^$DATE)"
+  local _warnings=$(echo "$dirtyJSON" | grep ^$DATE)
+  [ -n "$_warnings" ] && WARNINGS="${WARNINGS}${_warnings}"$'\n'
 }
 
 main() {
@@ -129,33 +157,43 @@ main() {
     exit 1
   }
 
+  HOST_LONG=$(hostname -f)
+  HOST=$(echo $HOST_LONG | sed 's/\..*//g')
+
   # Determine SSL or TLS or none
   ENCRYPTION=""
-  checkSSL $FQDN && {
+  checkSSL && {
     encType="tls"
     useMongoSSL && encType="ssl"
     ENCRYPTION="--host $(hostname -f) --authenticationDatabase admin --${encType} --${encType}AllowInvalidCertificates"
   }
 
   # Determine if login is required (needed for nologin / or STARTUP state)
-  mongo --quiet admin $LOGIN $ENCRYPTION <<< 'rs.conf()' >/dev/null 2>&1  || \
-  { mongo --quiet admin $ENCRYPTION <<< 'rs.conf()' >/dev/null 2>&1 && LOGIN=""; }
+  mongo --port $PORT --quiet admin $LOGIN $ENCRYPTION <<< 'rs.conf()' >/dev/null 2>&1  || \
+  { mongo --port $PORT --quiet admin $ENCRYPTION <<< 'rs.conf()' >/dev/null 2>&1 && LOGIN=""; }
   [ $? -ne 0 ] && helpInstall && exit 1
 
   VERSION=$(mongo --quiet admin $LOGIN $ENCRYPTION <<< 'JSON.stringify(db.version())')
   [[ "${VERSION#\"}" =~ ^2\. ]] && esc="'"
+  [[ "${VERSION#\"}" =~ ^3\.0 ]] && esc="'"
 
   sorted="${esc}(a,b) => a._id - b._id${esc}"
-  CONF=$(mongo --quiet admin $LOGIN $ENCRYPTION <<< "JSON.stringify(rs.conf().members.sort($sorted))")
-  STATUS=$(mongo --quiet admin $LOGIN $ENCRYPTION <<< "JSON.stringify(rs.status().members.sort($sorted))")
+  CONF=$(mongo --port $PORT --quiet admin $LOGIN $ENCRYPTION <<< "JSON.stringify(rs.conf().members.sort($sorted))")
+  STATUS=$(mongo --port $PORT --quiet admin $LOGIN $ENCRYPTION <<< "JSON.stringify(rs.status().members.sort($sorted))")
+
+  DATE=$(date '+%Y-%m-%d')
+  purifyJSON CONF
+  purifyJSON STATUS
+  echo -n "$WARNINGS"
 
   _ID=$(getDataFromJSON _id "$CONF")
   _HOST=$(getDataFromJSON host "$CONF"| shortHost)
   _HOST_LONG=$(getDataFromJSON host "$CONF")
 
   echo "$_HOST" | sort | uniq -c -d | grep . -q  && \
-    _HOST="$_HOST_LONG"
+    _HOST="$_HOST_LONG" && HOST="$HOST_LONG"
 
+  _HOST=$(echo "$_HOST" | colorize $HOST)
   _VOTES=$(getDataFromJSON votes "$CONF"| fixNull)
   _PRIORITY=$(getDataFromJSON priority "$CONF" | fixNull)
   _HIDDEN=$(getDataFromJSON hidden "$CONF"| fixHidden)
@@ -187,7 +225,7 @@ main() {
                         <(echo "$_OPTIME_I") \
   )
 
-  _STATE=$(getDataFromJSON stateStr "$STATUS"| sanitizeLine)
+  _STATE=$(getDataFromJSON stateStr "$STATUS"| sanitizeLine | colorize PRIMARY)
   _UP=$(getDataFromJSON health "$STATUS")
 
   _STATE=$(paste -d'\0' <(echo "$_STATE") \
@@ -204,7 +242,7 @@ main() {
   )
 
   ARRAY=$(echo -e "${HEAD}\n${ARRAY}" | adjustColumns | separateColumns )
-  lenLongestLine=$(echo "$ARRAY" | wc -L)
+  lenLongestLine=$(echo "$ARRAY" | fixColors | wc -L)
 
   topLine $lenLongestLine
   echo "$ARRAY" | fixLine $lenLongestLine
